ae58e7eede825821a8ad53a6307dd8b9511e0bf9
virupabandi <veerupaksha.bandi@gmail.com>
2021-09-08 16:34:36 +0000

    reverted after resolving the conflicts

--- vp.java
+++ vp.java
@@ -4,8 +4,10 @@ pull
 changes from local from child branch
 rebase, stash, stash poppop, cherry-pich, revert
 
-changes from local from master branch
+
+
 
 --making changes from local from my child, i thought i am in child branch-
 --changes to master from child--
+
 
