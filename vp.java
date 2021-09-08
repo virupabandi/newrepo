eedafaae35fc21fece9b5ad357feff36b35ed96d
virupabandi <veerupaksha.bandi@gmail.com>
2021-09-08 16:01:40 +0000

    resolving conflicts after cherry-pick

--- vp.java
+++ vp.java
@@ -5,4 +5,9 @@ changes from local from child branch
 rebase, stash, stash poppop, cherry-pich, revert
 changes from local from master branch
 
---making changes from local from my child, i thought i am in child branch
+
+
+--making changes from local from my child, i thought i am in child branch-
+--changes to master from child--
+
+
