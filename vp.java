cc5cf4ebb5dec69e0eee00535995ccd46bebdcb4
virupabandi <veerupaksha.bandi@gmail.com>
2021-09-09 10:45:27 +0000

    commit pop

--- vp.java
+++ vp.java
@@ -1,17 +1,12 @@
 Welcome to vpinfo 
 welcome 1st
 pull
-changes from local from child branch
-
-rebase, stash, stash poppop, cherry-pich
 
 rebase, stash, stash poppop, cherry-pich, revert
-changes from local from master branch
-
-
 
 --making changes from local from my child, i thought i am in child branch-
 --changes to master from child--
 
 
 
+:
